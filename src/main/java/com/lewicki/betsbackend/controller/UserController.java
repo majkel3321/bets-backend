ad710747d5a1e0fbe9be190d4d4faf1c198abcf7
majkel3321 <majkel3321@gmail.com>
2019-07-13 16:48:47 +0200

    initial implementation of userController

--- src/main/java/com/lewicki/betsbackend/controller/UserController.java
+++ src/main/java/com/lewicki/betsbackend/controller/UserController.java
@@ -3,11 +3,11 @@ package com.lewicki.betsbackend.controller;
 import com.lewicki.betsbackend.domain.User;
 import com.lewicki.betsbackend.domain.UserDto;
 import com.lewicki.betsbackend.mapper.UserMapper;
-import com.lewicki.betsbackend.service.Service;
+import com.lewicki.betsbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,28 +15,33 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private Service service;
+    private UserService userService;
 
     @Autowired
     private UserMapper userMapper;
 
     @GetMapping("getUser")
     public User getUser(@RequestParam Long id) {
-        return service.getUser(id).orElse(new User("empty", "empty"));
+        return userService.getUser(id).orElse(new User("username", "password","email"));
     }
 
     @GetMapping("getUsers")
     public List<User> getAllUsers() {
-        return service.getUsers();
+        return userService.getUsers();
     }
 
-    @PostMapping("createUser")
+    @PostMapping(value = "createUser",consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createUser(@RequestBody UserDto userDto) {
-        service.addUser(userMapper.mapToUser(userDto));
+        userService.addUser(userMapper.mapToUser(userDto));
     }
 
     @DeleteMapping("deleteUser")
     public void deleteUser(@RequestParam Long id) {
-        service.deleteUser(id);
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("login")
+    public User logIn(@RequestParam String username, @RequestParam String password){
+        return userService.logIn(username,password).orElse(new User("username","password","email"));
     }
 }
