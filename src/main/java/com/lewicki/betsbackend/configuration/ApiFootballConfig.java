54ed8d9f6f2dd81e106457534f1d4b0825d95916
majkel3321 <majkel3321@gmail.com>
2019-07-30 19:56:03 +0200

    configuration classes added

--- src/main/java/com/lewicki/betsbackend/configuration/ApiFootballConfig.java
+++ src/main/java/com/lewicki/betsbackend/configuration/ApiFootballConfig.java
@@ -1,4 +1,16 @@
 package com.lewicki.betsbackend.configuration;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
 public class ApiFootballConfig {
+
+    @Value("${football.api.key}")
+    private String apiFootballKey;
+
+    @Value("${football.api.endpoint}")
+    private String apiFootballEndpoint;
 }
--- src/main/java/com/lewicki/betsbackend/configuration/LiveOddsConfig.java
+++ src/main/java/com/lewicki/betsbackend/configuration/LiveOddsConfig.java
@@ -1,4 +1,16 @@
 package com.lewicki.betsbackend.configuration;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
 public class LiveOddsConfig {
+
+    @Value("${odds.live.key}")
+    private String liveOddsKey;
+
+    @Value("${odds.live.endpoint}")
+    private String liveOddsEndpoint;
 }
